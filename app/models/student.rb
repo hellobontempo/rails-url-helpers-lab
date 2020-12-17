2a96cdf9e4a8cd18e2d42f4c77a1c6b84fcb662a
claire <clairebontempo@gmail.com>
2020-12-16 17:04:07 -0800

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 class Student < ActiveRecord::Base
+  after_initialize :init
+
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def init
+    self.active = false if self.active.nil?
+  end
+
+  def status
+      if self.active == true
+        "active"
+      else
+        "inactive"
+      end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create([{first_name: 'Claire', last_name: 'Bontempo'}, {first_name: "Dominic", last_name:"Distefano"}])
